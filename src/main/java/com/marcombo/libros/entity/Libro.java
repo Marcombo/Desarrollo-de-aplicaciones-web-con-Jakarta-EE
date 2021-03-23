636c4113fc00806d65ce706de8175537af42cf52
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2021-03-23 09:47:04 -0300

    se agregaron mas atributos a la entidad Libro

--- src/main/java/com/marcombo/libros/entity/Libro.java
+++ src/main/java/com/marcombo/libros/entity/Libro.java
@@ -1,33 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.marcombo.libros.entity;
 
+import java.math.BigDecimal;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "Libros")
+@Getter
+@Setter
 public class Libro {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
+    @Column(name = "LibroID")
+    private Long libroId;
 
-    public Long getId() {
-        return id;
-    }
+    @Column(name = "Titulo")
+    private String titulo;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-    
-    
+    @Column(name = "Descripcion")
+    private String descripcion;
+
+    @Column(name = "Isbn")
+    private String isbn;
+
+    @Column(name = "Editorial")
+    private String editorial;
+
+    @Column(name = "Autor")
+    private String autor;
+
+    @Column(name = "Precio")
+    private BigDecimal precio;
+
 }
