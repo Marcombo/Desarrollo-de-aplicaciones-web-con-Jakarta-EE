b2ab46a547d4a7c2a323ed838596c3c76a8301cf
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2021-03-23 10:31:00 -0300

    implementacion de metodos create, update, delete y findById

--- src/main/java/com/marcombo/libros/service/LibroService.java
+++ src/main/java/com/marcombo/libros/service/LibroService.java
@@ -1,23 +1,39 @@
-
 package com.marcombo.libros.service;
 
 import com.marcombo.libros.entity.Libro;
 import java.util.List;
+import java.util.Optional;
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
+import javax.transaction.Transactional;
 
 @ApplicationScoped
 public class LibroService {
-
+    
     @PersistenceContext(unitName = "libreriaPU")
     private EntityManager entityManager;
-
-    public Libro findById(Long id) {
-        return new Libro();
+    
+    @Transactional
+    public Libro create(Libro libro) {
+        entityManager.persist(libro);
+        return libro;
     }
-
+    
+    @Transactional
+    public void update(Libro libro) {
+        entityManager.merge(libro);
+    }
+    
+    @Transactional
+    public void delete(Long id) {
+        findById(id).ifPresent(libro -> entityManager.remove(libro));
+    }
+    
+    public Optional<Libro> findById(Long id) {
+        return Optional.ofNullable(entityManager.find(Libro.class, id));
+    }
+    
     public List<Libro> findAll() {
         String consulta = "SELECT l FROM Libro l";
         return entityManager.createQuery(consulta).getResultList();
